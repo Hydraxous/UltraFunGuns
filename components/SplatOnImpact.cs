f696d37d7aa6cf299c73935be6cac260d1c74340
Hydraxous <robo-trey@outlook.com>
2022-09-14 21:35:14 -0700

    getting closer

--- components/SplatOnImpact.cs
+++ components/SplatOnImpact.cs
@@ -7,9 +7,10 @@ namespace UltraFunGuns
     public class SplatOnImpact : MonoBehaviour
     {
         EnemyIdentifier enemy;
-        private float velocityToSplatThreshold = 50.0f; //TODO IGBalancing
+        public float velocityToSplatThreshold = 50.0f; //TODO IGBalancing
         public float invincibilityTimer = 0.75f;
         private float timeElapsed = 0.0f;
+        private int collisions = 0;
 
         void Start()
         {
@@ -23,6 +24,7 @@ namespace UltraFunGuns
 
         private void OnCollisionEnter(Collision collision)
         {
+            ++collisions;
             if (timeElapsed > invincibilityTimer)
             {
                 if (collision.relativeVelocity.magnitude >= velocityToSplatThreshold)
@@ -31,9 +33,12 @@ namespace UltraFunGuns
                 }
                 else
                 {
-                    GetComponent<Rigidbody>().isKinematic = true;
-                    Destroy(this.GetComponent<SplatOnImpact>());
+                    GetComponent<Rigidbody>().isKinematic = true;       
                 }
+            }
+            if(collisions > 20)
+            {
+                Destroy(this.GetComponent<SplatOnImpact>());
             }
         }
     }
