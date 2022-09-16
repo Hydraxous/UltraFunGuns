1a4a8929a82593441fa340353b28d307ea6b6d6c
Hydraxous <robo-trey@outlook.com>
2022-09-16 14:25:11 -0700

    Organization and optimization fixes

---
+++ Components/Weapons/EggToss.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace UltraFunGuns
+{
+    public class EggToss : UltraFunGunBase
+    {
+        private GameObject thrownEggPrefab;
+        private Animator eggThrowAnimator;
+
+        public override void InitializeWeaponVariables()
+        {
+            eggThrowAnimator = GetComponent<Animator>();
+            HydraLoader.prefabRegistry.TryGetValue("ThrownEgg", out thrownEggPrefab);
+            fireDelayPrimary = 1.2f;
+            fireDelaySecondary = 1.2f;
+        }
+
+        public override void FirePrimary()
+        {
+            
+        }
+
+        public override void FireSecondary()
+        {
+            
+        }
+    }
+}
--- HydraLoader.cs
+++ HydraLoader.cs
@@ -1,30 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UltraFunGuns.Properties;
 
 namespace UltraFunGuns
 {
     public static class HydraLoader
     {
+        private static List<CustomAssetPrefab> assetsToRegister = new List<CustomAssetPrefab>();
 
-        public static Dictionary<string, DataFile> dataRegistry = new Dictionary<string, DataFile>();
+        private static List<CustomAssetData> dataToRegister = new List<CustomAssetData>();
+
+        public static Dictionary<string, UnityEngine.Object> dataRegistry = new Dictionary<string, UnityEngine.Object>();
 
         public static Dictionary<string, GameObject> prefabRegistry = new Dictionary<string, GameObject>();
+
+        private static AssetBundle assetBundle;
 
         public static bool dataRegistered = false;
         public static bool assetsRegistered = false;
 
-        public static bool RegisterAll()
+        public static bool RegisterAll(byte[] bundle)
         {
             try
             {
+                Debug.Log("HydraLoader: loading assets");
+                assetBundle = AssetBundle.LoadFromMemory(bundle);
                 RegisterDataFiles();
                 RegisterCustomAssets();
+                Debug.Log("HydraLoader: loading complete");
                 return true;
             }
             catch (Exception e)
             {
+                Debug.Log("HydraLoader: loading failed");
                 return false;
             }
         }
@@ -33,20 +41,19 @@ namespace UltraFunGuns
         {
             if (!dataRegistered)
             {
-                GyroRotator.GyroRotatorData innerGyroStabilizer = new GyroRotator.GyroRotatorData(1.0f, Vector3.forward, 0.004f, 3f, 40.66f); //InnerGyroBearing of SonicReverberator gun
-                dataRegistry.Add("InnerGyroBearing", innerGyroStabilizer);
-
-                GyroRotator.GyroRotatorData middleGyro = new GyroRotator.GyroRotatorData(1.2f, new Vector3(1, 0, 0), 0.004f, 3.5f, -53.58f); //MiddleGyro of SonicReverberator
-                dataRegistry.Add("MiddleGyro", middleGyro);
-
-                GyroRotator.GyroRotatorData innerGyro = new GyroRotator.GyroRotatorData(1.5f, Vector3.back, 0.004f, 4f, -134.3f); //InnerGyro of sonic reverbgun
-                dataRegistry.Add("InnerGyro", innerGyro);
-
-                GyroRotator.GyroRotatorData moyai = new GyroRotator.GyroRotatorData(1.5f, Vector3.one, 0.005f, 15f, -248.5f);
-                dataRegistry.Add("Moyai", moyai);
-
+                foreach (CustomAssetData assetData in dataToRegister)
+                {
+                    if (assetData.dataFile != null)
+                    {
+                        dataRegistry.Add(assetData.name, assetData.dataFile);
+                    }
+                    else
+                    {
+                        dataRegistry.Add(assetData.name, assetBundle.LoadAsset(assetData.name));
+                    }
+                    
+                }
                 dataRegistered = true;
-                Debug.Log("data registered!");
             }
         }
 
@@ -54,33 +61,51 @@ namespace UltraFunGuns
         {
             if (!assetsRegistered)
             {
-                AssetBundle assetBundle = AssetBundle.LoadFromMemory(UltraFunGunsResources.UltraFunGuns); //CHANGE TO LOAD FROM MEMORY AND USE RESOURCES.
-
-                //Moyai gun fx projectile.
-                GameObject sonicReverberation = assetBundle.LoadAsset<GameObject>("SonicReverberation");
-                SonicReverberatorExplosion reverberation = sonicReverberation.AddComponent<SonicReverberatorExplosion>();
-                prefabRegistry.Add("SonicReverberation", sonicReverberation);
-
-                //Moyai Gun
-                GameObject sonicReverberator = assetBundle.LoadAsset<GameObject>("SonicReverberator");
-                SonicReverberator sonicGun = sonicReverberator.AddComponent<SonicReverberator>();
-                WeaponIcon srIcon = sonicReverberator.AddComponent<WeaponIcon>();
-                srIcon.weaponIcon = assetBundle.LoadAsset<Sprite>("SonicReverberator_weaponIcon");
-                srIcon.glowIcon = assetBundle.LoadAsset<Sprite>("SonicReverberator_glowIcon");
-                sonicGun.vB_standard = assetBundle.LoadAsset<AudioClip>("vineBoom_Standard");
-                sonicGun.vB_loud = assetBundle.LoadAsset<AudioClip>("vineBoom_Loud");
-                sonicGun.vB_loudest = assetBundle.LoadAsset<AudioClip>("vineBoom_Loudest");
-                srIcon.variationColor = 0;
-                sonicReverberator.SetActive(false);
-                sonicGun.bang = sonicReverberation;
-                
-                prefabRegistry.Add("SonicReverberator", sonicReverberator);
-
+                foreach (CustomAssetPrefab asset in assetsToRegister)
+                {
+                    GameObject newPrefab = assetBundle.LoadAsset<GameObject>(asset.name);
+                    for (int i = 0; i < asset.modules.Length; i++)
+                    {
+                        newPrefab.AddComponent(asset.modules[i].GetType());
+                    }
+                    prefabRegistry.Add(asset.name, newPrefab);
+                }
                 assetsRegistered = true;
-                Debug.Log("assets registered!");
+            }
+        }
+
+        public class CustomAssetPrefab
+        {
+            public Component[] modules;
+            public string name;
+
+            public CustomAssetPrefab(string assetName, Component[] componentsToAdd)
+            {
+                this.name = assetName;
+                this.modules = componentsToAdd;
+                assetsToRegister.Add(this);
+            }
+        }
+
+        public class CustomAssetData
+        {
+            public string name;
+            public DataFile dataFile;
+
+            public CustomAssetData(string dataName, DataFile dataFile) //For loading custom script data
+            {
+                this.name = dataName;
+                this.dataFile = dataFile;
+                dataToRegister.Add(this);
+            }
+
+            public CustomAssetData(string dataName) //For loading general assets
+            {
+                this.name = dataName;
+                dataToRegister.Add(this);
             }
         }
     }
 
-    public class DataFile { }
+    public class DataFile : UnityEngine.Object { }
 }
