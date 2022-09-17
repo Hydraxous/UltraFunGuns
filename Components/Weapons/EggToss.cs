098fb75eee442a0b9485602bd3f0c82167943496
Hydraxous <robo-trey@outlook.com>
2022-09-16 21:26:30 -0700

    Egg!

--- Components/Weapons/EggToss.cs
+++ Components/Weapons/EggToss.cs
@@ -9,18 +9,27 @@ namespace UltraFunGuns
     {
         private GameObject thrownEggPrefab;
         private Animator eggThrowAnimator;
-
+        public float forceMultiplier = 2.0f;
         public override void InitializeWeaponVariables()
         {
             eggThrowAnimator = GetComponent<Animator>();
             HydraLoader.prefabRegistry.TryGetValue("ThrownEgg", out thrownEggPrefab);
-            fireDelayPrimary = 1.2f;
+            fireDelayPrimary = 0.6f;
             fireDelaySecondary = 1.2f;
+        }
+
+        public override void DoAnimations()
+        {
+            eggThrowAnimator.SetBool("CanShoot", CanShoot(timeToFirePrimary));
         }
 
         public override void FirePrimary()
         {
-            
+            eggThrowAnimator.SetTrigger("Shoot");
+            GameObject newThrownEgg = GameObject.Instantiate<GameObject>(thrownEggPrefab,mainCam.TransformPoint(0,0,0.5f),Quaternion.identity);
+            newThrownEgg.transform.forward = mainCam.forward;
+            newThrownEgg.GetComponent<Rigidbody>().velocity = mainCam.TransformDirection(0, 0, 1 * forceMultiplier);
+
         }
 
         public override void FireSecondary()
