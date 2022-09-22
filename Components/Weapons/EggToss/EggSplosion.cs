b1b14423d45cdc06848698dd1a4d6b32d635a3e9
Hydraxous <robo-trey@outlook.com>
2022-09-22 15:21:10 -0700

    organization

---
+++ Components/Weapons/EggToss/EggSplosion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace UltraFunGuns
+{
+    public class EggSplosion : MonoBehaviour
+    {
+
+        public float explosionSize = 3.5f;
+
+        private void Start()
+        {
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, explosionSize, transform.position, Mathf.Infinity);
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
