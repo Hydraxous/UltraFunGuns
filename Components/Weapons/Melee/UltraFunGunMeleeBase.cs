d1f65c4d90589fb59d55c4c346230981ac2c70fd
Hydraxous <robo-trey@outlook.com>
2022-10-05 08:35:31 -0700

    after release

---
+++ Components/Weapons/Melee/UltraFunGunMeleeBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+namespace UltraFunGuns
+{
+    public class UltraFunGunMeleeBase : UltraFunGunBase
+    {
+
+        public float maxHitAngle;
+        public float maxRange;
+
+        public float damageMultiplier;
+        public float forceMultiplier;
+
+        public override Dictionary<string, ActionCooldown> SetActionCooldowns()
+        {
+            return base.SetActionCooldowns();
+        }
+
+        public override void GetInput()
+        {
+            
+        }
+
+        public virtual void DoMeleeAttack()
+        {
+
+        }
+
+        public override void DoAnimations()
+        {
+
+        }
+    }
+}
