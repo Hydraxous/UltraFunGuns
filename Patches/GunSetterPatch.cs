2b592f364560eb6b2c988eef9db5310d25a553f4
Hydraxous <robo-trey@outlook.com>
2023-01-17 19:48:33 -0800

    UFG interaction interface

--- Patches/GunSetterPatch.cs
+++ Patches/GunSetterPatch.cs
@@ -6,21 +6,6 @@ using HarmonyLib;
 
 namespace UltraFunGuns
 {
-    //[HarmonyPatch(typeof(ShopZone), "Start")]
-    public static class WeaponShopPatch
-    {
-        public static void Postfix(ShopZone __instance)
-        {
-            /*PLAN
-             * 1. Make tip box smaller -> set tip box anchorMin.y to 0.2f
-             * 2. Create button for UFG menu under the Main Menu as a parent.-> 
-             Meh...
-             */
-        }
-
-
-    }
-
     [HarmonyPatch(typeof(GunSetter),"ResetWeapons")]
     public static class GunSetterPatch
     {
